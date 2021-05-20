fdb6ae7ef6d8508a9a1492c5388c0005bf11055e
Brian Seo <bkseo@ucsd.edu>
2021-05-20 16:55:38 -0700

    finished part1

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -2,3 +2,18 @@
  * @jest-environment jsdom
  */
 
+ import { TestScheduler } from 'jest';
+import { pushToHistory } from '../scripts/router.js';
+
+ describe('test length of history stack', () => {
+    test('length of settings branch', () => {
+        expect(pushToHistory("settings", 0).length).toBe(2);
+    })
+    test('length of entry branch', () => {
+        expect(pushToHistory("entry", 1).length).toBe(3);
+    })
+    test('length of default branch', () => {
+        expect(pushToHistory("", 0).length).toBe(4);
+    })
+ });
+
